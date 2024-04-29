0b22cfb31d0a474243149f157fc5162f15d4e508
Newton <u1804090@student.cuet.ac.bd>
2024-04-30 00:36:06 +0600

    Add error handling middleware in index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Koa = require('koa');
 const {koaBody} = require('koa-body');
 const router = require('./router');
+const errorHandler = require('./handler/errorHandler');
 const responseHandler = require('./handler/responseHandler');
 
 const app = new Koa();
-
+app.use(errorHandler);
 app.use(responseHandler());
 app.use(koaBody({includeUnparsed: true}));
 app.use(router.routes());
